4c904c4096a20cf45b9e2db2828c9c08a0eabe7f
RAJSABARI K P <rajsabari74@gmail.com>
2023-04-03 22:18:13 +0530

    RAJSABARI| Main Java file

--- app/src/main/java/com/rajsabari/Utils/MainApplication.java
+++ app/src/main/java/com/rajsabari/Utils/MainApplication.java
@@ -1,2 +1,26 @@
-package com.rajsabari.Utils;public class MainApplication {
+package com.rajsabari.Utils;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+
+        RealmConfiguration value = new RealmConfiguration.Builder()
+                .name("value")
+                .schemaVersion(1)
+                .allowWritesOnUiThread(true)
+                .deleteRealmIfMigrationNeeded()
+                .build();
+
+
+        Realm.setDefaultConfiguration(value);
+    }
 }
